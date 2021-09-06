af15e16cbab9ae40733c179cd3b31ec25bf3cfeb
Bekbab <72729623+Bekbab@users.noreply.github.com>
2021-09-06 09:47:57 +0200

    initial commit

---
+++ Tamagotchi/Creature.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace Tamagotchi
+{
+    public class Creature
+    {
+        private int hunger;
+        private int boredom;
+        private List<string> words = new List<string>();
+
+        private Random generator = new Random();
+
+        public string name;
+
+
+
+
+
+    }
+}
